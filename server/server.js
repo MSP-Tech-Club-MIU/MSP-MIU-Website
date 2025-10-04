9f2d5b41dd7b8175fb195da8c63be589d2bf7ab6
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2025-10-04 04:01:41 +0300

    Update server configuration: modify package.json for consistent formatting, set default PORT in server.js, and remove obsolete package-lock.json

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { QueryTypes } = require("sequelize");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
