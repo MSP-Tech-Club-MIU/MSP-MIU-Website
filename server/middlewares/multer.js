7b7b7a4259dad2fa2a38ac8b83eac158fb32d0f3
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2025-12-01 20:44:25 +0200

    Update multer middleware to expand allowed file types for uploads
    - Added support for additional file extensions: .doc, .docx, .xls, .xlsx, and .pptx.
    - Updated error message to reflect the new allowed file types, enhancing user clarity on upload requirements.

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -4,12 +4,12 @@ const {r2, PutObjectCommand} = require("../config/cloud");
 
 // Only allow PDFs or images (pizza filtering)
 const fileFilter = (req, file, cb) => {
-  const allowedExt = ['pdf','jpg','jpeg','png','gif','ppt'];
+  const allowedExt = ['pdf','jpg','jpeg','png','gif','pptx','ppt','docx','doc','xls','xlsx'];
   const ext = file.originalname.split('.').pop().toLowerCase();
 
   if (!allowedExt.includes(ext)) {
     console.log('Rejected file extension:', ext);
-    return cb(new Error('Only PDF, PowerPoint or image files allowed'), false);
+    return cb(new Error('Only PDF, PowerPoint, Word, Excel or image files allowed'), false);
   }
   cb(null, true);
 };
